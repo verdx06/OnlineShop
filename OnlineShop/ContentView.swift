549582ede1809e19b9702e61c36a4756c018a6f5
verdx <berkut589243@gmail.com>
2024-09-10 17:15:54 +0700

    Initial Commit

---
+++ OnlineShop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnlineShop
+//
+//  Created by Виталий Багаутдинов on 10.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnlineShop/OnlineShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineShopApp.swift
+//  OnlineShop
+//
+//  Created by Виталий Багаутдинов on 10.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct OnlineShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
