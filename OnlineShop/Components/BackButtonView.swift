43070528a7981e0e3b975634d83af9111036db15
verdx <berkut589243@gmail.com>
2024-09-13 06:51:08 +0700

    add detailView Button

---
+++ OnlineShop/Components/BackButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  BackButtonView.swift
+//  OnlineShop
+//
+//  Created by Виталий Багаутдинов on 13.09.2024.
+//
+
+import SwiftUI
+
+struct BackButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackButtonView()
+}
---
+++ OnlineShop/Views/DetailView/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  OnlineShop
+//
+//  Created by Виталий Багаутдинов on 13.09.2024.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
