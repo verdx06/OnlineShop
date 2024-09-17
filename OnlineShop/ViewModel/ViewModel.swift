d550db92522eff21e90083fd1bf16c0856a870e1
verdx <berkut589243@gmail.com>
2024-09-17 17:30:25 +0700

    добавил функцию апдейта, добавил обновление корзины в реальном времени, добавил общую цену на продукты, кооторые добавлены в корзину

--- OnlineShop/ViewModel/ViewModel.swift
+++ OnlineShop/ViewModel/ViewModel.swift
@@ -11,26 +11,52 @@ import FirebaseFirestore
 final class ViewModel: ObservableObject {
     // MARK: - Properties
     private let database = Firestore.firestore().collection("shop")
+    @Published var cartItem: [Product] = []
+    
+    var titleCount: Int {
+        cartItem.reduce(0) { $0 + ($1.price * ($1.count ?? 0))}
+    }
+    
+    init() {
+        fetchItems()
+    }
+    
+    private func fetchItems() {
+        database.addSnapshotListener { snapshot, error in
+            if let document = snapshot?.documents {
+                self.cartItem = document.compactMap{ document -> Product? in
+                    try? document.data(as: Product.self)
+                }
+            }
+            else {
+                print("Error \(error!)")
+                return
+            }
+        }
+    }
     
     // MARK: - Methods
     func toggleFavorite(product: Product) {
-        if let id = product.id {
-            database.document(id).updateData(["favorite" : !product.favorite])
-        }
+        updateDatabase(product: product, data: ["favorite" : !product.favorite])
     }
     
     func addProdutInCart(product:Product) {
-        if let id = product.id {
-            database.document(id).updateData(["count" : (product.count ?? 0) + 1])
-        }
+        updateDatabase(product: product, data: ["count" : (product.count ?? 0) + 1])
     }
     
     func minusProdutInCart(product:Product) {
-        if let id = product.id {
-            database.document(id).updateData(["count" : (product.count ?? 0) - 1])
-        }
+        let maxMinus = max((product.count ?? 0) - 1, 1)
+        updateDatabase(product: product, data: ["count" : maxMinus])
     }
     
+    func deleteProdutInCart(product:Product) {
+        updateDatabase(product: product, data: ["count" : 0])
+    }
     
+    private func updateDatabase(product: Product, data: [String : Any]) {
+        if let id = product.id {
+            database.document(id).updateData(data)
+        }
+    }
     
 }
