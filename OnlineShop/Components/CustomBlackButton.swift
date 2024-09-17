162eb94805df9ed265537913383252e15085788d
verdx <berkut589243@gmail.com>
2024-09-17 17:29:28 +0700

    Добавил отдельное вью для кнопок

---
+++ OnlineShop/Components/CustomBlackButton.swift
@@ -0,0 +1,28 @@
+//
+//  CustomBlackButton.swift
+//  OnlineShop
+//
+//  Created by Виталий Багаутдинов on 17.09.2024.
+//
+
+import SwiftUI
+
+struct CustomBlackButton: View {
+    
+    let title: String
+    let action: () -> ()
+    
+    var body: some View {
+        Button {
+            action()
+        } label: {
+            Text(title)
+                .frame(maxWidth: .infinity)
+                .frame(height: 65)
+                .bold()
+                .foregroundColor(.white)
+                .background(Color.black)
+                .clipShape(Capsule())
+        }
+    }
+}
