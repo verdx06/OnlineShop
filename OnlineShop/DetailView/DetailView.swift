f6fe299be36ce6546fa1f037d0b4e4c675fadc25
verdx <berkut589243@gmail.com>
2024-09-18 02:46:16 +0700

    Подправил код

--- OnlineShop/DetailView/DetailView.swift
+++ OnlineShop/DetailView/DetailView.swift
@@ -1,10 +1,3 @@
-//
-//  DetailView.swift
-//  OnlineShop
-//
-//  Created by Виталий Багаутдинов on 13.09.2024.
-//
-
 import SwiftUI
 import FirebaseFirestore
 
@@ -14,43 +7,23 @@ struct DetailView: View {
     @FirestoreQuery(collectionPath: "shop") private var items: [Product]
     @EnvironmentObject var viewModel: ViewModel
     @State var isAlert: Bool = false
+    let widthImage: CGFloat = UIScreen.main.bounds.width
+    let heightImage: CGFloat = UIScreen.main.bounds.height / 1.5
     
     var body: some View {
         
         ZStack (alignment: .topLeading){
             VStack {
                 if let url = URL(string: product.image) {
-                    CardImageView(url: url, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 1.5)
+                    CardImageView(url: url, width: widthImage, height: heightImage)
                         .ignoresSafeArea()
                         .shadow(color: .black, radius: 10)
                     
                 }
                 
-                VStack(alignment: .leading) {
-                    HStack {
-                        Text(product.name)
-                            .font(.system(size: 15))
-                            .bold()
-                        
-                        Spacer()
-                        
-                        Text("\(product.price)")
-                            .font(.system(size: 15))
-                            .bold()
-                    }
-                    
-                    Text(product.description)
-                        .font(.system(size: 13))
-                        .fixedSize(horizontal: false, vertical: true)
-                    
-                    Spacer()
-                    
-                    
-                    detailViewWithProduct
-
-                    
-                }
-                .padding(.horizontal)
+                NamePriceDescription
+                
+                detailViewWithProduct
                 
             }
             .navigationBarBackButtonHidden(true)
@@ -74,57 +47,7 @@ struct DetailView: View {
     
 }
 
-extension DetailView {
-    
-    private var detailViewWithProduct: some View {
-        Group {
-            if checkCount(), let count = product.count {
-                ZStack {
-                    Capsule()
-                        .frame(maxWidth: .infinity)
-                        .frame(height: 65)
-                        .bold()
-                        .foregroundColor(.black)
-                        HStack {
-                            Image(systemName: "minus")
-                                .foregroundColor(.white)
-                                .font(.system(size: 20))
-                                .bold()
-                                .onTapGesture {
-                                    viewModel.minusProdutInCart(product: product)
-                                }
-                            
-                            Spacer()
-                            
-                            Text("\(count)")
-                                .bold()
-                                .foregroundColor(.white)
-                                .font(.system(size: 20))
-                            
-                            Spacer()
-                            
-                            Image(systemName: "plus")
-                                .foregroundColor(.white)
-                                .font(.system(size: 20))
-                                .bold()
-                                .onTapGesture {
-                                    viewModel.addProdutInCart(product: product)
-                                }
-                        }
-                        .padding(.horizontal, 40)
-                        Text("В корзине")
-                            .foregroundColor(.gray)
-                            .font(.system(size: 12))
-                            .offset(y: 20)
-                }
-            } else {
-                CustomBlackButton(title: "Добавить продукт") {
-                    viewModel.addProdutInCart(product: product)
-                }
-            }
-        }
-    }
-}
+
 
 
 #Preview {
