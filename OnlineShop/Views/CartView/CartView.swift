95bd8a332720a7521e4be4702ac6b02fe0193798
verdx <berkut589243@gmail.com>
2024-09-25 02:59:09 +0700

    Немного сократил код

--- OnlineShop/Views/CartView/CartView.swift
+++ OnlineShop/Views/CartView/CartView.swift
@@ -13,12 +13,31 @@ struct CartView: View {
                     ForEach(items.filter{ $0.count ?? 0 >= 1 }) { item in
                         ProductViewForCart(product: item)
                     }
+                    TotalAndButton
                 }else {
                     NoProductViewForCart()
                         .transition(AnyTransition.opacity.animation(.easeInOut))
                 }
             }
-            
+        }.navigationTitle("Корзина")
+        
+        
+    }
+    
+    func checkCount() -> Bool {
+        for item in items {
+            if item.count ?? 0 >= 1 {
+                return true
+            }
+        }
+        return false
+    }
+}
+
+extension CartView {
+    
+    var TotalAndButton: some View {
+        Group {
             Text("Total: \(viewModel.titleCount)$")
                 .bold()
                 .padding(.bottom)
@@ -34,19 +53,8 @@ struct CartView: View {
                     .background(Color.black)
                     .clipShape(Capsule())
             })
-                .padding(.horizontal, 30)
-        }.navigationTitle("Корзина")
-        
-        
-    }
-    
-    func checkCount() -> Bool {
-        for item in items {
-            if item.count ?? 0 >= 1 {
-                return true
-            }
+            .padding(.horizontal, 30)
         }
-        return false
     }
 }
 
